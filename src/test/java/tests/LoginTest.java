710b2bbef691a6ff4163d3082e3b8827fddcd930
DmitryKshes <dmitriykshes@gmail.com>
2025-06-14 23:38:44 +0300

    created SauceDemo project

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import base.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void successLoginTest() {
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Login failed");
+    }
+}
